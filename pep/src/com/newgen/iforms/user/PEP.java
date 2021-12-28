51385a505b80d580de2ed4a03f37fcf737b54651
Kel Morgan <kelmorgan18@gmail.com>
2021-12-28 14:42:38 +0100

    latest code from office intellij

--- pep/src/com/newgen/iforms/user/PEP.java
+++ pep/src/com/newgen/iforms/user/PEP.java
@@ -11,56 +11,63 @@ import com.newgen.worksteps.*;
 import org.apache.log4j.Logger;
 
 public class PEP implements IFormListenerFactory, Constants {
-	Logger logger = LogGenerator.getLoggerInstance(PEP.class);
+	private final Logger logger = LogGenerator.getLoggerInstance(PEP.class);
 
 	@Override
 	public IFormServerEventHandler getClassInstance(IFormReference ifr) {
+		logger.info("Code got here!!");
 		IFormServerEventHandler objActivity = null;
-		String workStep = Form.getCurrentWorkStep(ifr);
-		logger.info("workStep: "+workStep);
-	
-		if (Shared.isProcessName(ifr, pepProcessName)) {
-			switch (workStep) {
-				case branchInitiatorWs:{
-					objActivity = new BranchInitiator();
-					break;
-				}
-				case branchVerifierWs:{
-					objActivity = new BranchVerifier();
-					break;
-				}
-				case acoWs:{
-					objActivity = new Aco();
-					break;
-				}
-				case amlWs:{
-					objActivity = new Aml();
-					break;
-				}
-				case lineExecutiveWs:{
-					objActivity = new LineExecutive();
-					break;
-				}
-				case ccoWs:{
-					objActivity = new Cco();
-					break;
-				}
-				case reworkWs:{
-					objActivity = new Rework();
-					break;
-				}
-				case amlInitiatorWs:{
-					objActivity = new AmlInitiator();
-					break;
-				}
-				case exitWs:
-				case discardWs:
-				case queryWs:{
-					objActivity = new Exit();
-					break;
-				}
+		try {
 
+			String workStep = Form.getCurrentWorkStep(ifr);
+			logger.info("workStep: " + workStep);
+
+			if (Shared.isProcessName(ifr, pepProcessName)) {
+				switch (workStep) {
+					case branchInitiatorWs: {
+						objActivity = new BranchInitiator();
+						break;
+					}
+					case branchVerifierWs: {
+						objActivity = new BranchVerifier();
+						break;
+					}
+					case acoWs: {
+						objActivity = new Aco();
+						break;
+					}
+					case amlWs: {
+						objActivity = new Aml();
+						break;
+					}
+					case lineExecutiveWs: {
+						objActivity = new LineExecutive();
+						break;
+					}
+					case ccoWs: {
+						objActivity = new Cco();
+						break;
+					}
+					case reworkWs: {
+						objActivity = new Rework();
+						break;
+					}
+					case amlInitiatorWs: {
+						objActivity = new AmlInitiator();
+						break;
+					}
+					case exitWs:
+					case discardWs:
+					case queryWs: {
+						objActivity = new Exit();
+						break;
+					}
+
+				}
 			}
+		}
+		catch (Exception e){
+			logger.error("Exception occurred in pep home class "+ e.getMessage());
 		}
 		return objActivity;
 	}
